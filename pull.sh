48734ff965abc6a9f42184a51a1c4784af5c9fd3
AnAncientForce <zflyway45@protonmail.com>
2023-07-26 15:18:47 +0100

    new up

--- pull.sh
+++ pull.sh
@@ -8,14 +8,15 @@ cd "$(dirname "$0")"
 
 # Check if it's a git repository before performing git pull
 if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    git pull
+    git pull origin main
     echo "Repository updated."
 else
-    echo "Not a git repository. Initializing a new git repository..."
+    echo "Initializing a new git repository..."
     git init
     git remote add origin https://github.com/AnAncientForce/sysZ.git
-    git fetch
-    git checkout main
+    git fetch origin main
+    git checkout -b main --track origin/main
+    echo "Git repository set up. Repository is ready."
 fi
 
 # Return to the original directory
