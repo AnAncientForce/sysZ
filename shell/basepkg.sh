eaded2dcbba01df7a1755f6f66b98ee38c45d07e
AnAncientForce <zflyway45@protonmail.com>
2023-07-27 18:38:27 +0100

    ye

--- shell/basepkg.sh
+++ shell/basepkg.sh
@@ -54,15 +54,7 @@ yay_packages=(
 yay
 pacman
 
-# Run specific function is specified
-if [ "$1" == "--function" ]; then
-    shift  # Shift the arguments to skip the "--function" flag
-    function_name="$1"
-    shift  # Shift again to skip the function name
 
-    # Call the specified function
-    "$function_name"
-fi
 
 
 install_missing_packages() {
