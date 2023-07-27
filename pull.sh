9b7b78c67818e53a1c7033909f7d00eaf93a7c63
AnAncientForce <zflyway45@protonmail.com>
2023-07-27 17:52:00 +0100

    ok

--- pull.sh
+++ pull.sh
@@ -100,7 +100,12 @@ fi
 read -p "
 Setup dark mode?
 (y/n): " choice
+
 if [ "$choice" = "y" ]; then
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this script with sudo or as root."
+        exit 1
+    fi
     echo "Setting up QT_QPA_PLATFORMTHEME in /etc/environment..."
     echo '(QT_QPA_PLATFORMTHEME="qt5ct")' > /etc/environment
 fi
