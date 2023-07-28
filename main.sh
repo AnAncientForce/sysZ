b689cdaa3ac875ebf18bd0d014b52601e3a4d3c4
AnAncientForce <zflyway45@protonmail.com>
2023-07-28 14:02:23 +0100

    ok

--- main.sh
+++ main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Get the directory of the script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Read the value from the JSON file
-value=$(jq -r '.use_animations' "config.json")
+value=$(jq -r '.use_animations' "$script_dir/config.json")
 
 # Check if the value is true
 if [ "$value" = "true" ]; then
     i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
     echo "picom_with_animations"
-else
+elif [ "$value" = "false" ]; then
     i3-msg 'exec picom -b --blur-background --corner-radius 4 --vsync;'
     echo "picom_without_animations"
 fi
