cd465baa21325c1eac3e1378b2a94f02713e575e
AnAncientForce <zflyway45@protonmail.com>
2023-07-28 13:19:09 +0100

    test

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 picom_without_animations() {
     i3-msg 'exec picom -b --blur-background --corner-radius 4 --vsync;'
+    echo "picom_without_animations"
 }
 picom_with_animations() {
     i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
+    echo "picom_with_animations"
 }
 setup() {
     sh /home/$(whoami)/sysZ/shell/setup.sh
