2e1e861a3e682ad877cfd1b410623488fc5d6ad6
AnAncientForce <zflyway45@protonmail.com>
2023-07-28 12:36:14 +0100

    updates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import sys
 import subprocess
 import os
+import json
 
 def stop_loading():
     root.destroy()
@@ -65,6 +66,24 @@ def update():
 
 
 
+def setup():
+    if check_value_from_json('use_animations'):
+        print("Animations are enabled.")
+        subprocess.run("i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'", shell=True)
+    else:
+        print("Animations are disabled.")
+        subprocess.run("i3-msg 'exec picom -b --blur-background --corner-radius 4 --vsync;'", shell=True)
+    subprocess.Popen(["sh", os.path.expanduser("~/sysZ/shell/setup.sh")])
+
+
+def check_value_from_json(key):
+    with open('config.json', 'r') as file:
+        data = json.load(file)
+        if key in data:
+            return data[key]
+        else:
+            return False
+
 def control():
     clear_tk_elements(root)
     # root.attributes('-fullscreen', True) 
@@ -73,6 +92,50 @@ def control():
 
     label = ttk.Label(root, text="sysZ | Control Panel", font=("Arial", 36), background=root['bg'])
     label.pack(pady=100)
+
+    # --- SETTINGS
+
+    # Function to update the config file
+    def update_config():
+        with open('config.json', 'w') as configfile:
+            config['use_animations'] = use_animations.get()
+            config['splashEnabled'] = splash_enabled.get()
+            json.dump(config, configfile)
+
+    # Function to execute specific code based on the config value
+    def execute_code():
+        if config['use_animations']:
+            print("Animations enabled")
+            
+        else:
+            print("Animations disabled")
+            
+
+    # Create the tkinter window
+    # root = tk.Tk()
+
+    # Read the configuration file
+    with open('config.json', 'r') as configfile:
+        config = json.load(configfile)
+
+    # Create a checkbox button for using animations
+    use_animations = tk.BooleanVar(value=config.get('use_animations', False))
+    checkbox = tk.Checkbutton(root, text="Use animations", variable=use_animations, command=update_config)
+    checkbox.pack(pady=10)
+
+    # Create a checkbox button for enabling splash
+    splash_enabled = tk.BooleanVar(value=config.get('splashEnabled', False))
+    checkbox_splash = tk.Checkbutton(root, text="Enable Splash", variable=splash_enabled, command=update_config)
+    checkbox_splash.pack(pady=10)
+
+    # Create a button to execute code based on the config value
+    execute_button = tk.Button(root, text="Execute Code", command=execute_code)
+    execute_button.pack(pady=10)
+
+    # Run the tkinter event loop
+    # root.mainloop()
+
+    # --- SETTING END
 
     terminal_button = ttk.Button(root, text="Open Terminal", command=lambda: subprocess.Popen(["alacritty", "&"], shell=True))
     terminal_button.pack(pady=10)
--- shell/setup.sh
+++ shell/setup.sh
@@ -2,7 +2,7 @@
 killall -9 picom polybar
 i3-msg 'exec feh --bg-fill ~/sysZ/bg.*;'
 i3-msg 'exec polybar -c ~/sysZ/conf/polybar.ini;'
-i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
+# i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
 i3-msg "exec sox ~/sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
 i3-msg "reload"
 # i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
