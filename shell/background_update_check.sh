c560bdbb4d543a9f9beab697d433ec3cb01ca4c4
AnAncientForce <zflyway45@protonmail.com>
2023-07-29 17:36:54 +0100

    OK

--- shell/background_update_check.sh
+++ shell/background_update_check.sh
@@ -2,10 +2,9 @@ current_dir=$(pwd)
 cd "/home/$(whoami)/sysZ"
 
 if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    if git diff --quiet; then
-        echo "No changes detected."
-    else
+    if [ "$(git status --porcelain)" ]; then
         python /home/$(whoami)/sysZ/main.py update_confirmation
+    else
+        echo "No changes detected."
     fi
 fi
-# I have made an update test
