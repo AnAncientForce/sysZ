10258e2cb0a05f6e2b97212d00039c6f74aa05d3
AnAncientForce <zflyway45@protonmail.com>
2023-07-29 17:29:05 +0100

    ok

--- shell/background_update_check.sh
+++ shell/background_update_check.sh
@@ -1,7 +1,10 @@
 current_dir=$(pwd)
-
 cd "/home/$(whoami)/sysZ"
 
 if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    python /home/$(whoami)/sysZ/main.py update_confirmation
+    if git diff --quiet; then
+        echo "No changes detected."
+    else
+        python /home/$(whoami)/sysZ/main.py update_confirmation
+    fi
 fi
