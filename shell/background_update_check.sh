f2fce478378b181be7a81c1f35cc9d4a649b706d
AnAncientForce <zflyway45@protonmail.com>
2023-07-29 17:40:59 +0100

    ok

--- shell/background_update_check.sh
+++ shell/background_update_check.sh
@@ -2,9 +2,10 @@ current_dir=$(pwd)
 cd "/home/$(whoami)/sysZ"
 
 if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    if [ "$(git status --porcelain)" ]; then
+    git remote update >/dev/null 2>&1
+    if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
         python /home/$(whoami)/sysZ/main.py update_confirmation
     else
-        echo "No changes detected."
+        echo "No updates available."
     fi
 fi
