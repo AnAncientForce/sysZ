2cd7e7ba79dabdc751c5028380bc370a64a5875d
AnAncientForce <zflyway45@protonmail.com>
2023-07-31 17:24:55 +0100

    ok

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def update():
     try:
         prepare_image_rotation(root)
         debugTxt("Checking github repository for updates")
-        subprocess_thread = threading.Thread(target=lambda: execute_shell_script("sysZ/shell/non_sudo_update.sh"))
+        subprocess_thread = threading.Thread(target=lambda: execute_shell_script("~/sysZ/shell/non_sudo_update.sh"))
         subprocess_thread.start()
         global setup_pending
         setup_pending = True
