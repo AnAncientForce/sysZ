ebd1951ea46825a86d87946e98c16690071e3d5d
AnAncientForce <zflyway45@protonmail.com>
2023-07-31 17:47:02 +0100

    ok

--- shell/setup.sh
+++ shell/setup.sh
@@ -12,7 +12,7 @@ i3-msg "exec polybar -c $sysZ/conf/polybar.ini;"
 i3-msg "exec sh $sysZ/shell/background_update_check.sh;"
 i3-msg "exec sox $sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
 i3-msg "reload"
-sleep 0.5
+sleep 0.25
 
 # & DOSE NOT HOLD UP SHELL
 # >/dev/null 2>&1 & REDIRECTS OUTPUT TO NULL
