8ddcc9150783fa532b5300454735a7741953ddab
AnAncientForce <zflyway45@protonmail.com>
2023-07-31 17:37:51 +0100

    ok

--- shell/setup.sh
+++ shell/setup.sh
@@ -2,11 +2,17 @@
 sysZ="/home/$(whoami)/sysZ"
 
 killall -9 polybar
-feh --bg-fill /home/$(whoami)/sysZ/bg.*
-polybar -c "$sysZ/conf/polybar.ini" >/dev/null 2>&1 &
-sh "$sysZ/shell/background_update_check.sh" & # & Because if there is an update, the setup screen will never go away
-sox "$sysZ/sfx/Sys_Camera_SavePicture.flac" -d >/dev/null 2>&1 &
+#feh --bg-fill /home/$(whoami)/sysZ/bg.*
+#polybar -c "$sysZ/conf/polybar.ini" >/dev/null 2>&1 &
+#sh "$sysZ/shell/background_update_check.sh" & # & Because if there is an update, the setup screen will never go away
+#sox "$sysZ/sfx/Sys_Camera_SavePicture.flac" -d >/dev/null 2>&1 &
+
+i3-msg "exec feh --bg-fill $sysZ/bg.*;"
+i3-msg "exec polybar -c $sysZ/conf/polybar.ini;"
+i3-msg "exec sh $sysZ/shell/background_update_check.sh;"
+i3-msg "exec sox $sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
 i3-msg "reload"
+sleep 0.5
 
 # & DOSE NOT HOLD UP SHELL
 # >/dev/null 2>&1 & REDIRECTS OUTPUT TO NULL
