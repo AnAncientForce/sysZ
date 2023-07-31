dbcdff035820971b59b0172875835067e0df6498
AnAncientForce <zflyway45@protonmail.com>
2023-07-31 16:57:55 +0100

    ok

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+sysZ="/home/$(whoami)/sysZ"
+
 killall -9 polybar
-feh --bg-fill ~/sysZ/bg.* &
-polybar -c ~/sysZ/conf/polybar.ini >/dev/null 2>&1 &
-sh /home/$(whoami)/sysZ/shell/background_update_check.sh
-sox ~/sysZ/sfx/Sys_Camera_SavePicture.flac -d &
+feh --bg-fill "$sysZ/bg.*" &
+polybar -c "$sysZ/conf/polybar.ini" >/dev/null 2>&1 &
+sh "$sysZ/shell/background_update_check.sh" &
+sox "$sysZ/sfx/Sys_Camera_SavePicture.flac" -d >/dev/null 2>&1 &
 i3-msg "reload"
 
-#i3-msg 'exec feh --bg-fill ~/sysZ/bg.*;' &
-#i3-msg 'exec polybar -c ~/sysZ/conf/polybar.ini;' &
-#i3-msg "exec sh /home/$(whoami)/sysZ/shell/background_update_check.sh;" &
-#i3-msg "exec sox ~/sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
+# i3-msg 'exec feh --bg-fill ~/sysZ/bg.*;' &
+# i3-msg 'exec polybar -c ~/sysZ/conf/polybar.ini;' &
+# i3-msg "exec sh /home/$(whoami)/sysZ/shell/background_update_check.sh;" &
+# i3-msg "exec sox ~/sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
 
 # i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
 # i3-msg 'exec picom -b --blur-background --backend glx --animations --animation-for-open-window zoom --corner-radius 4 --vsync;'
