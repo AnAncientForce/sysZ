9776344ebcf65b5ef9ef84fd7cff059bdefc383b
AnAncientForce <zflyway45@protonmail.com>
2023-08-01 12:54:14 +0100

    Updates

--- other/backup.sh
+++
@@ -1,3 +0,0 @@
-mkdir ~/$USER/sysZ/recovery/
-pacman -Qet > ~/$USER/sysZ/recovery/packages.txt
-pacman -Qqm > ~/$USER/sysZ/recovery/yay.txt
--- other/cu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp "conf/i3/config" "/home/$(whoami)/.config/i3/"
-cp "conf/kitty/kitty.conf" "/home/$(whoami)/.config/kitty/"
-cp "conf/alacritty.yml" "/home/$(whoami)/.config/"
