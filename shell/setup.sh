c6b80c3a904958fe659f0d093006ab17b6be795c
AnAncientForce <zflyway45@protonmail.com>
2023-08-03 19:14:32 +0100

    Changes

--- shell/setup.sh
+++ shell/setup.sh
@@ -13,7 +13,7 @@ i3-msg "exec polybar -c $sysZ/conf/polybar.ini;"
 i3-msg "exec sh $sysZ/shell/background_update_check.sh;"
 i3-msg "exec sox $sysZ/sfx/Sys_Camera_SavePicture.flac -d;"
 i3-msg "reload"
-betterlockscreen ~/sysZ/bg
+betterlockscreen -u /home/$(whoami)/sysZ/bg
 
 # & DOSE NOT HOLD UP SHELL
 # >/dev/null 2>&1 & REDIRECTS OUTPUT TO NULL
