85f7ea9c3913a37458bf1e7efe48220cf3f86f7f
AnAncientForce <zflyway45@protonmail.com>
2023-08-04 14:52:39 +0100

    Changes

--- shell/u.sh
+++ shell/u.sh
@@ -5,23 +5,10 @@ if [ "$EUID" -eq 0 ]; then
 else
     sysZ="/home/$(whoami)/sysZ"
 fi
-rm -f "$sysZ/shell/pull.sh"
-
-# Store the current directory
-current_dir=$(pwd)
-
-# Change directory to sysZ root
-cd "$(dirname "$0")"
-
-# Check if it's a git repository and perform git pull or initialize a new git repository
-if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    git pull origin main
-    echo "Repository updated."
-else
-    echo "Initializing a new git repository..."
-    git clone --branch main https://github.com/AnAncientForce/sysZ.git .
-    echo "Git repository set up. Repository is ready."
+read -p "Delete pull.sh?" choice
+if [ "$choice" = "y" ]; then
+    rm -f "$sysZ/shell/pull.sh"
 fi
-
+git pull
 chmod +x $sysZ/pull.sh
 cd $sysZ/shell
