2bcd2e3cc531fe48371e55e3f82569dc9dee2344
AnAncientForce <zflyway45@protonmail.com>
2023-08-04 14:55:46 +0100

    c

--- shell/u.sh
+++ shell/u.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 sysZ=""
+
+repo_pull() {
+    # Store the current directory
+    current_dir=$(pwd)
+
+    # Change directory to sysZ root
+    cd "$(dirname "$0")"
+
+    # Check if it's a git repository before performing git pull
+    if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        git pull origin main
+        echo "Repository updated."
+    else
+        echo "Initializing a new git repository..."
+        git init
+        git remote add origin https://github.com/AnAncientForce/sysZ.git
+        git fetch origin main
+        git checkout -b main --track origin/main
+        echo "Git repository set up. Repository is ready."
+    fi
+}
+
 if [ "$EUID" -eq 0 ]; then
     sysZ="/home/$SUDO_USER/sysZ"
 else
@@ -9,6 +31,6 @@ read -p "Delete pull.sh?" choice
 if [ "$choice" = "y" ]; then
     rm -f "$sysZ/shell/pull.sh"
 fi
-git pull
-chmod +x $sysZ/pull.sh
+repo_pull
+chmod +x $sysZ/shell/pull.sh
 cd $sysZ/shell
