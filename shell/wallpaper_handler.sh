a351f4d38978614a59ff41ba1c08c479a3048ad6
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 20:58:55 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -15,27 +15,35 @@ send_pause_command() {
     echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
 }
 
+# Function to send the play command to mpv via socat
+send_play_command() {
+    echo '{"command": ["cycle", "play"]}' | socat - /tmp/mpvsocket
+}
+
 # Wait for the mpv socket to be available
 while [ ! -e /tmp/mpvsocket ]; do
     sleep 0.1
 done
 
+# Variable to store the last focused window name
+last_window_name=$(xdotool getwindowfocus getwindowname)
+
 # Main loop to monitor focus changes
 while true; do
-    if is_alacritty_or_i3_focused; then
-        echo "Alacritty or i3 is in focus."
-        playerctl -p mpv status | grep -q "Playing"
-        if [[ $? -ne 0 ]]; then
+    current_window_name=$(xdotool getwindowfocus getwindowname)
+
+    if [[ $current_window_name != $last_window_name ]]; then
+        if is_alacritty_or_i3_focused; then
+            echo "Alacritty or i3 is in focus."
+            send_play_command
+        else
+            echo "Alacritty and i3 are not in focus."
             send_pause_command
         fi
-    else
-        echo "Alacritty and i3 are not in focus."
-        playerctl -p mpv status | grep -q "Paused"
-        if [[ $? -ne 0 ]]; then
-            send_pause_command
-        fi
+
+        last_window_name=$current_window_name
     fi
 
     # Small sleep to reduce CPU usage
-    sleep 5
+    sleep 0.5
 done
