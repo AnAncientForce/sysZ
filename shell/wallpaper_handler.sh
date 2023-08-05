d10a98e8f670436893afbca719b912f231047c12
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 18:17:00 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -5,14 +5,19 @@ contains_window() {
     xprop -root | grep -q -i "$1"
 }
 
-# Function to check if mpv is running and playing an .mp4 video
+# Check if the mpv process is running and playing an .mp4 video
 is_mpv_playing() {
-    playerctl status | grep -q "Playing"
+    pgrep -x "mpv" &>/dev/null && pgrep -f "\.mp4" &>/dev/null
 }
 
-# Function to pause or resume mpv
-toggle_mpv_playback() {
-    playerctl play-pause
+# Pause the mpv player
+pause_mpv() {
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.mpv /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+}
+
+# Play or resume the mpv player
+play_mpv() {
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.mpv /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
 }
 
 # Main loop to monitor focus changes
@@ -23,7 +28,13 @@ while true; do
     # Check if any window other than alacritty is in focus
     if ! xprop -root | grep -q -i "alacritty"; then
         if is_mpv_playing; then
-            toggle_mpv_playback
+            # Pause mpv if playing
+            pause_mpv
+        fi
+    else
+        if is_mpv_playing; then
+            # Resume mpv if paused
+            play_mpv
         fi
     fi
 done
