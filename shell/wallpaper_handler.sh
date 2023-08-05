d59bd097988b04a7245a7293f056e39dfa1d2cd0
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 19:25:01 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -3,39 +3,28 @@
 # Variable to store the playing state (0 for paused, 1 for playing)
 is_playing=0
 
-# Function to check if alacritty is in focus and the window title matches "username@hostname"
 is_alacritty_focused() {
     local active_window_title=$(xprop -id "$(xdotool getactivewindow)" WM_NAME | sed -r 's/WM_NAME\(\w+\) = "(.*)"$/\1/')
     [[ $active_window_title == "$(whoami)@$HOSTNAME:"* ]]
 }
 
-# Function to check if the current workspace is blank (empty)
-is_blank_workspace() {
-    local current_workspace=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
-    [[ -z $current_workspace ]]
-}
-
-# Function to send the pause command to mpv via socat
 send_pause_command() {
     echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
 }
 
-# Main loop to monitor focus changes
 while true; do
-    if is_alacritty_focused || is_blank_workspace; then
-        echo "Alacritty or a blank workspace is in focus."
+    if is_alacritty_focused; then
+        echo "Good"
         if [[ $is_playing -eq 0 ]]; then
             send_pause_command
             is_playing=1
         fi
     else
-        echo "Neither Alacritty nor a blank workspace is in focus."
+        echo "Bad"
         if [[ $is_playing -eq 1 ]]; then
             send_pause_command
             is_playing=0
         fi
     fi
-
-    # Small sleep to reduce CPU usage
     sleep 0.5
 done
