9bfbf35b2ebcdf7f357899dc6c45eba7a0278383
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 18:10:59 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# Function to check if a window name contains a specific string
-contains_window() {
-    xprop -root | grep -q -i "$1"
-}
-
-# Function to check if mpv is running and playing an .mp4 video
-is_mpv_playing() {
-    pgrep -f "mpv.*\.mp4" &>/dev/null
-}
-
 # Main loop to monitor focus changes
 while true; do
     # Wait for the next focus event
     xdotool selectwindow >/dev/null 2>&1
 
     # Check if any window other than alacritty is in focus
-    if ! contains_window "alacritty"; then
-        if is_mpv_playing; then
+    if ! xprop -root | grep -q -i "alacritty"; then
+        if pgrep -f "mpv.*\.mp4" &>/dev/null; then
             # Pause mpv if playing
-            pkill -SIGSTOP mpv
+            xdotool key --clearmodifiers "p"
         fi
+    fi
+
+    # Focus on alacritty or an empty workspace
+    if xprop -root | grep -q -i "alacritty"; then
+        xdotool search --name "Alacritty" windowactivate
+    else
+        xdotool key --clearmodifiers "Super+1" # Replace with your workspace switching keybinding
     fi
 done
