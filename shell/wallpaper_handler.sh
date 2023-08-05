7edddcffde5b30ea52e4ce54ec7488ab700ca793
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 18:45:09 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Check if xdotool is installed
-if ! command -v xdotool &>/dev/null; then
-    echo "Error: xdotool is not installed. Please install it using your package manager."
-    exit 1
-fi
+# Function to check if alacritty is in focus and the window title matches "username@hostname"
+is_alacritty_focused() {
+    local active_window_title=$(xprop -id "$(xdotool getactivewindow)" WM_NAME | sed -r 's/WM_NAME\(\w+\) = "(.*)"$/\1/')
+    [[ $active_window_title == "$(whoami)@$(hostname):"* ]]
+}
 
-# Define the Alacritty window class name (it may vary depending on your setup)
-ALACRITTY_WINDOW_CLASS="Alacritty"
+# Main loop to monitor focus changes
+while true; do
+    if is_alacritty_focused; then
+        echo "Alacritty is in focus and has the correct window title."
+    else
+        echo "Alacritty is not in focus or has an incorrect window title."
+    fi
 
-# Check if Alacritty window is in focus
-if xdotool getwindowfocus getwindowname | grep -q "$ALACRITTY_WINDOW_CLASS"; then
-    echo "Alacritty is in focus."
-else
-    echo "Alacritty is not in focus."
-fi
+    # Small sleep to reduce CPU usage
+    sleep 0.5
+done
