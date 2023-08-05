b8353820380cb79485660e246e3ac786a6c5b0ff
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 18:26:54 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Function to check if mpv is running and playing an .mp4 video
 is_mpv_playing() {
     pgrep -x "mpv" &>/dev/null && pgrep -f "\.mp4" &>/dev/null
+}
+
+# Function to pause the mpv video
+pause_mpv() {
+    echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
 }
 
 # Main loop to monitor focus changes
 while true; do
     # Wait for the next focus event
-    xdotool selectwindow >/dev/null 2>&1
-
-    # Check if any window other than alacritty is in focus
-    if ! xprop -root | grep -q -i "alacritty"; then
-        if is_mpv_playing; then
-            echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
+    xprop -root -spy | while read -r; do
+        # Check if any window other than alacritty is in focus
+        if ! xprop -root | grep -q -i "alacritty"; then
+            if is_mpv_playing; then
+                # Pause mpv if playing
+                pause_mpv
+            fi
         fi
-    fi
+    done
 done
