5df37253c4509641f3d7e183b4ab63b33b231848
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 19:29:51 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -3,29 +3,38 @@
 # Variable to store the playing state (0 for paused, 1 for playing)
 is_playing=0
 
+# Function to check if alacritty is in focus and the window title matches "username@hostname"
 is_alacritty_focused() {
     local active_window_title=$(xprop -id "$(xdotool getactivewindow)" WM_NAME | sed -r 's/WM_NAME\(\w+\) = "(.*)"$/\1/')
     [[ $active_window_title == "$(whoami)@$HOSTNAME:"* ]]
 }
 
+# Function to send the pause command to mpv via socat
 send_pause_command() {
     echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
 }
 
-sleep 2
+# Wait for the mpv socket to be available
+while [ ! -e /tmp/mpvsocket ]; do
+    sleep 0.1
+done
+
+# Main loop to monitor focus changes
 while true; do
     if is_alacritty_focused; then
-        echo "Good"
+        echo "Alacritty is in focus."
         if [[ $is_playing -eq 0 ]]; then
             send_pause_command
             is_playing=1
         fi
     else
-        echo "Bad"
+        echo "Alacritty is not in focus."
         if [[ $is_playing -eq 1 ]]; then
             send_pause_command
             is_playing=0
         fi
     fi
+
+    # Small sleep to reduce CPU usage
     sleep 0.5
 done
