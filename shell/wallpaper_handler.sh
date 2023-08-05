2b9afe1bfcc740464ecac8ed3e8e0f50681a971d
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 20:12:06 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -11,14 +11,6 @@ send_pause_command() {
     echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
 }
 
-# Function to check if there are any windows in the current workspace
-has_windows_in_workspace() {
-    local active_window_id=$(xdotool getactivewindow)
-    local current_workspace=$(xprop -id "$active_window_id" _NET_WM_DESKTOP | awk '{print $2}')
-    local windows_in_workspace=$(xdotool search --onlyvisible --desktop "$current_workspace" "" 2>/dev/null)
-    [[ -n "$windows_in_workspace" ]]
-}
-
 # Wait for the mpv socket to be available
 while [ ! -e /tmp/mpvsocket ]; do
     sleep 0.1
@@ -26,14 +18,14 @@ done
 
 # Main loop to monitor focus changes
 while true; do
-    if is_alacritty_focused || has_windows_in_workspace; then
-        echo "Alacritty is in focus or there are windows in the workspace."
+    if is_alacritty_focused; then
+        echo "Alacritty is in focus."
         playerctl -p mpv status | grep -q "Playing"
         if [[ $? -ne 0 ]]; then
             send_pause_command
         fi
     else
-        echo "Alacritty is not in focus and there are no windows in the workspace."
+        echo "Alacritty is not in focus."
         playerctl -p mpv status | grep -q "Paused"
         if [[ $? -ne 0 ]]; then
             send_pause_command
