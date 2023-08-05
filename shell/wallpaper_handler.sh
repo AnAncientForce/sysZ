d16354e3c76f8574af7c78e992649ff65f849c88
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 18:25:37 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-PLAY=false
+
+is_mpv_playing() {
+    pgrep -x "mpv" &>/dev/null && pgrep -f "\.mp4" &>/dev/null
+}
+
+# Main loop to monitor focus changes
 while true; do
-    if [ "$(xdotool getwindowfocus getwindowname)" == "i3" ] && [ $PLAY == false ]; then
-        echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
-        PLAY=true
-    elif [ "$(xdotool getwindowfocus getwindowname)" != "i3" ] && [ $PLAY == true ]; then
-        echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
-        PLAY=false
+    # Wait for the next focus event
+    xdotool selectwindow >/dev/null 2>&1
+
+    # Check if any window other than alacritty is in focus
+    if ! xprop -root | grep -q -i "alacritty"; then
+        if is_mpv_playing; then
+            echo '{"command": ["cycle", "pause"]}' | socat - /tmp/mpvsocket
+        fi
     fi
-    sleep 1
 done
