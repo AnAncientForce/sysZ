0032520bacf4db97137197d2a0e8d9b301f2ead3
AnAncientForce <zflyway45@protonmail.com>
2023-08-05 17:43:02 +0100

    x

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -5,26 +5,42 @@ contains_window() {
     xprop -root | grep -q -i "$1"
 }
 
-# Command to set the live wallpaper
-LIVE_WALLPAPER_COMMAND="xwinwrap -fs -ov -ni -nf -un -s -d -o 1.0 -debug -- mpv -wid WID --loop --no-audio $sysZ/vid.mp4"
+# Function to check if mpv is running
+is_mpv_running() {
+    pgrep -f "mpv" &>/dev/null
+}
 
-# Main loop
-while true; do
-    # Get the currently focused window name
-    FOCUSED_WINDOW=$(xdotool getwindowfocus getwindowname)
+# Set initial state (not in focus)
+WINDOW_IN_FOCUS=false
 
-    # Check if the terminal (alacritty) is in focus
-    if contains_window "alacritty"; then
+# Function to pause or resume the live wallpaper based on focus state
+toggle_live_wallpaper() {
+    if $WINDOW_IN_FOCUS; then
         # Terminal is in focus, allow the wallpaper to play
-        if ! pgrep -f "mpv" &>/dev/null; then
-            # Start the live wallpaper if it's not already running
-            # $LIVE_WALLPAPER_COMMAND &
+        if ! is_mpv_running; then
+            # Resume the live wallpaper if it was paused
+            pkill -SIGCONT mpv
         fi
     else
         # Terminal is not in focus, pause the wallpaper if it's running
-        pkill -SIGSTOP mpv
+        if is_mpv_running; then
+            pkill -SIGSTOP mpv
+        fi
+    fi
+}
+
+# Main loop to monitor focus changes
+while true; do
+    # Wait for the next focus event
+    xdotool selectwindow >/dev/null 2>&1
+
+    # Check if the terminal (alacritty) is in focus
+    if contains_window "alacritty"; then
+        WINDOW_IN_FOCUS=true
+    else
+        WINDOW_IN_FOCUS=false
     fi
 
-    # Small sleep to reduce CPU usage
-    sleep 0.5
+    # Pause or resume the live wallpaper based on the focus state
+    toggle_live_wallpaper
 done
