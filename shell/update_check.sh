bee78dd533f7cecb94bb97c56f066c3b001d11f4
AnAncientForce <zflyway45@protonmail.com>
2023-08-06 13:37:29 +0100

    c

---
+++ shell/update_check.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+user_home=""
+json_file=""
+sysZ=""
+
+if [ "$EUID" -eq 0 ]; then
+    sysZ="/home/$SUDO_USER/sysZ"
+    json_file="/home/$SUDO_USER/.config/sysZ/config.json"
+    user_home="/home/$SUDO_USER"
+else
+    sysZ="/home/$(whoami)/sysZ"
+    json_file="/home/$(whoami)/.config/sysZ/config.json"
+    user_home="/home/$(whoami)"
+fi
+
+checkJson() {
+    if [ -f "$json_file" ]; then
+        value=$(jq -r ".$1" "$json_file")
+        if [ "$value" = "true" ]; then
+            return 0
+        else
+            return 1
+        fi
+    else
+        echo "JSON file not found: $json_file"
+        return 2
+    fi
+}
+
+if ! checkJson "ignore_updates"; then
+    current_dir=$(pwd)
+    cd $sysZ
+
+    if [ -d ".git" ] || git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        git remote update >/dev/null 2>&1
+        if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+            return true
+        else
+            return false
+        fi
+    fi
+    return false
+fi
