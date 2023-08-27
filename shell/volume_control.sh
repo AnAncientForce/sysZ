2779f606eb40ac1a5f6fdc456df1a986b2d55ba6
AnAncientForce <zflyway45@protonmail.com>
2023-08-27 14:08:43 +0100

    c

--- shell/volume_control.sh
+++ shell/volume_control.sh
@@ -3,7 +3,8 @@
 MAX_VOLUME=100
 
 adjust_volume() {
-    volume_percent=$(amixer -D pulse sget Master | grep 'Right:' | awk -F'[][]' '{print $2}')
+    local sink_id=$(pactl list short sinks | awk '{print $1; exit}')
+    local volume_percent=$(pactl list sinks | grep -A 10 "Sink #$sink_id" | grep 'Volume:' | awk '{print $5}' | sed 's/%//')
 
     if [ "$1" = "up" ]; then
         new_volume=$((volume_percent + 10))
@@ -17,11 +18,12 @@ adjust_volume() {
         new_volume=0
     fi
 
-    amixer -D pulse sset Master "$new_volume"% >/dev/null
+    pactl set-sink-volume "$sink_id" "$new_volume%" >/dev/null
     echo "$new_volume"
 }
 
 if [ "$1" = "up" ] || [ "$1" = "down" ]; then
     new_volume=$(adjust_volume "$1")
-    echo "Volume: $new_volume%"
+    echo "$new_volume"
+    dunstify -t 2000 "Volume Adjusted" "$new_volume%"
 fi
