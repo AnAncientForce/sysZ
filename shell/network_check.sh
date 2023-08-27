773ce8505418ff5df896a26ee5895f2276ed161b
AnAncientForce <zflyway45@protonmail.com>
2023-08-27 13:35:11 +0100

    c

--- shell/network_check.sh
+++ shell/network_check.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-Color_Off='\033[0m'
-BBlack='\033[1;30m' BRed='\033[1;31m' BGreen='\033[1;32m' BYellow='\033[1;33m'
-BBlue='\033[1;34m' BPurple='\033[1;35m' BCyan='\033[1;36m' BWhite='\033[1;37m'
-
 if ! ip link show | grep -q "state UP"; then
-    echo -e ${BRed}"No internet" ${Color_Off}
+    echo "No internet"
 fi
