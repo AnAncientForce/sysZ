b1e5db1fe3546049963ff4b6304dc4a12c2ab2b0
AnAncientForce <zflyway45@protonmail.com>
2023-09-03 23:32:53 +0100

    c

--- shell/merge.sh
+++ shell/merge.sh
@@ -5,11 +5,11 @@ textdoc1="/home/$(whoami)/sysZ/conf/i3"
 textdoc2="/home/$(whoami)/.config/i3/config"
 
 # Find the line number of the "=============================" line in TextDoc2.txt
-delimiter_line_number=$(grep -n "# ==================================================================================================== End ====================================================================================================" "$textdoc2" | cut -d ':' -f 1)
+delimiter_line_number=$(grep -n "#=====EDIT_UNDER_THIS_LINE=====" "$textdoc2" | cut -d ':' -f 1)
 
 # If the delimiter is found, proceed with the replacement
 if [ -n "$delimiter_line_number" ]; then
-    # Extract everything above the delimiter from TextDoc2.txt
+    # Extract everything above and including the delimiter from TextDoc2.txt
     head -n "$delimiter_line_number" "$textdoc2" >temp.txt
 
     # Append the content of TextDoc1.txt to temp.txt
