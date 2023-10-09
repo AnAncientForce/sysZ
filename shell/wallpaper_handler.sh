727de34dc3e145499b555f512ab0311a2bd27664
AnAncientForce <zflyway45@protonmail.com>
2023-10-09 14:06:22 +0100

    c

--- shell/wallpaper_handler.sh
+++ shell/wallpaper_handler.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-app1() {
-    local active_window_name=$(xdotool getwindowfocus getwindowname)
-    [[ $active_window_name == "$(whoami)@$HOSTNAME:"* ]]
-}
+window_names=("$(whoami)@$HOSTNAME:" "Alacritty" "i3")
 
-app2() {
+is_window_focused() {
     local active_window_name=$(xdotool getwindowfocus getwindowname)
-    [[ $active_window_name == *"Alacritty"* ]]
-}
-
-is_i3_focused() {
-    local active_window_name=$(xdotool getwindowfocus getwindowname)
-    [[ $active_window_name == *"i3"* ]]
+    for name in "${window_names[@]}"; do
+        if [[ $active_window_name == *"$name"* ]]; then
+            return 0 # Window name matched
+        fi
+    done
+    return 1 # Window name didn't match any
 }
 
 send_pause_command() {
