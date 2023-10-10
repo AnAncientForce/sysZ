38004b49baf9718eba04666352d5408c16d3c56c
AnAncientForce <zflyway45@protonmail.com>
2023-10-10 20:55:13 +0100

    c

--- shell/pull.sh
+++ shell/pull.sh
@@ -609,16 +609,19 @@ wm_setup_func() {
     #if checkJson "prevent_sleeping"; then
     #    i3-msg "exec xset -dpms;"
     #fi
+    # alacritty -e bash -c "sudo systemctl start bluetooth.service; sudo systemctl enable bluetooth.service;"
 
     if checkJson "enable_bluetooth"; then
         if [ "$(systemctl is-enabled bluetooth.service)" = "disabled" ]; then
             echo -e ${BRed}"\n[!] Requesting authentication to change bluetooth state > enabled" ${Color_Off}
-            alacritty -e bash -c "sudo systemctl start bluetooth.service; sudo systemctl enable bluetooth.service;"
+            sudo systemctl start bluetooth.service
+            sudo systemctl enable bluetooth.service
         fi
     else
         if [ "$(systemctl is-enabled bluetooth.service)" = "enabled" ]; then
             echo -e ${BRed}"\n[!] Requesting authentication to change bluetooth state > disabled" ${Color_Off}
-            alacritty -e bash -c "sudo systemctl stop bluetooth.service; sudo systemctl disable bluetooth.service;"
+            sudo systemctl stop bluetooth.service
+            sudo systemctl disable bluetooth.service
         fi
     fi
     if [ -f "$user_home/.config/sysZ/autostart.sh" ]; then
