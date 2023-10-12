df075f52b938ee577aad3d634d5b243f2394fe84
AnAncientForce <zflyway45@protonmail.com>
2023-10-12 12:55:55 +0100

    c

--- shell/pull.sh
+++ shell/pull.sh
@@ -812,6 +812,38 @@ for arg in "$@"; do
         auto_sysZ_install=true
         valid_flag=true
         ;;
+    --mod)
+        # https://wiki.archlinux.org/title/Touchpad_Synaptics
+        # https://archlinux.org/pacman/pacman.conf.5.html
+
+        mkdir -p "$user_home/.config/emergency-restore"
+        read -p "t = touchpad, p = pacman (t/p): " choice
+
+        if [ "$choice" = "t" ]; then
+            read -p "y = install, n = uninstall (y/n): " choice
+            sudo cp "/etc/X11/xorg.conf.d/70-synaptics.conf" "$user_home/.config/emergency-restore/70-synaptics.conf"
+
+            if [ "$choice" = "y" ]; then
+                # sudo cp "$sysZ/conf/30-touchpad.conf" "/etc/X11/xorg.conf.d/30-touchpad.conf"
+                sudo cp "$sysZ/conf/70-synaptics.conf" "/etc/X11/xorg.conf.d/70-synaptics.conf"
+                echo -e "${BGreen}[*] Installed\n${Color_Off}"
+            elif [ "$choice" = "n" ]; then
+                # sudo rm "/etc/X11/xorg.conf.d/30-touchpad.conf"
+                sudo rm "/etc/X11/xorg.conf.d/70-synaptics.conf"
+                echo -e "${BGreen}[*] Removed\n${Color_Off}"
+            else
+                echo -e "${BRed}[*] Invalid choice. Please enter 'y' to install or 'n' to uninstall.\n${Color_Off}"
+            fi
+
+        elif [ "$choice" = "p" ]; then
+            sudo cp "/etc/pacman.conf" "$user_home/.config/emergency-restore/pacman.conf"
+            sudo cp "$sysZ/conf/pacman.conf" "/etc/pacman.conf"
+            echo -e "${BGreen}[*] Installed\n${Color_Off}"
+        else
+            echo -e "${BRed}[*] Invalid choice\n${Color_Off}"
+        fi
+        exit 0
+        ;;
     *) ;;
     esac
 done
