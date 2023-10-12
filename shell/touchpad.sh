f5560243b1f812f50f4f80cda8474b0afec2b02c
AnAncientForce <zflyway45@protonmail.com>
2023-10-12 12:06:42 +0100

    c

--- shell/touchpad.sh
+++ shell/touchpad.sh
@@ -14,10 +14,10 @@ fi
 read -p "y = install, n = uninstall (y/n): " choice
 
 if [ "$choice" = "y" ]; then
-    sudo cp "$sysZ/conf/70-synaptics.conf" "/usr/share/X11/xorg.conf.d/70-synaptics.conf"
+    sudo cp "$sysZ/conf/70-synaptics.conf" "/etc/X11/xorg.conf.d/70-synaptics.conf"
     echo "Installed"
 elif [ "$choice" = "n" ]; then
-    sudo rm "/usr/share/X11/xorg.conf.d/70-synaptics.conf"
+    sudo rm "/etc/X11/xorg.conf.d/70-synaptics.conf"
     echo "Removed"
 else
     echo "Invalid choice. Please enter 'y' to install or 'n' to uninstall."
